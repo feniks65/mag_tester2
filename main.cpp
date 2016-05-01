fa46318cb4bf4386bc5e9b8ebfe9d684dd877b73
Wojciech Wais <wojciech.wais@gmail.com>
2016-05-01 10:23:38 +0200

    new functions like countBlackPixs()

--- main.cpp
+++ main.cpp
@@ -6,14 +6,41 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+static const unsigned int Threshold = 180;
+
 using namespace std;
 using namespace cv;
+
+image.at<Vec3b>(Point(x,y)) = color; //wstawia w x,y jakąś barwę
+
+int* countBlackPixs(Mat inputImage)
+{
+	int *countedBlacks = new int[inputImage.cols];
+
+	for(int i=0; i < inputImage.cols; i++)
+	{
+		countedBlacks[i] = inputImage.rows - countNonZero(inputImage.col(i));
+	}
+
+	return countedBlacks;
+}
 
 Mat convertToGrayScale(Mat inputImage)
 {
 	Mat outputImage;
 
 	cvtColor(inputImage, outputImage, CV_BGR2GRAY);
+
+	return outputImage;
+}
+
+Mat convertToBinary(Mat inputImage)
+{
+	Mat outputImage;
+
+	//cvThreshold(inputImage, outputImage, 128, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+
+	outputImage = inputImage > Threshold;
 
 	return outputImage;
 }
@@ -30,19 +57,16 @@ Mat blur(Mat inputImage)
 Mat rotate90(Mat inputImage)
 {
 	Mat outputImage;
-	double angle = 90;  // or 270
 
-	Size src_sz = inputImage.size();
-	Size dst_sz(src_sz.height, src_sz.width);
-
-	int len = max(inputImage.cols, inputImage.rows);
-	Point2f center(len/2., len/2.);
-	Mat rot_mat = getRotationMatrix2D(center, angle, 1.0);
-	warpAffine(inputImage, outputImage, rot_mat, dst_sz);
+	outputImage = inputImage.t();
 
 	return outputImage;
 }
 
+Mat drawChart(Mat inputImage)
+{
+	
+}
 
 int main()
 {
@@ -50,6 +74,7 @@ int main()
 	const char *windowName = "probka";
 	struct stat sb;
 	Mat image;
+	int *countedBlacks = NULL;
 
 	if(stat(exampleSample, &sb) == -1)
 	{
@@ -64,9 +89,17 @@ int main()
 	//rotate90
 	image = rotate90(image);
 
+	//convert to binary
+	image = convertToBinary(image);
+
 	//display image
 	namedWindow(windowName, CV_WINDOW_NORMAL);
 	imshow(windowName, image);
+
+	countedBlacks = countBlackPixs(image);
+
+	for(int i = 0; i < image.cols; i++)
+		cout << "col=" << i << " ,val=" << countedBlacks[i] << endl;
 
 	while(true)
 	{
